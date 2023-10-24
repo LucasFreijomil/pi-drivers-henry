cc03df2718597386abb40130299210e8fbd83e98
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-10-24 11:00:17 -0300

    show-all-button

--- client/src/components/NavBar/Navbar.jsx
+++ client/src/components/NavBar/Navbar.jsx
@@ -1,7 +1,6 @@
 import Styles from "../NavBar/NavBar.module.css";
 import f1_logo from "../../assets/F1-logo.svg.png";
 
-
 const NavBar = () => {
   return (
     <div className={Styles.navBar}>
@@ -9,9 +8,14 @@ const NavBar = () => {
       <button className={Styles.navButton}>Home</button>
       <button className={Styles.navButton}>Form</button>
       <div className={Styles.searchContainer}>
-      <input type="text" placeholder="Search by name..." className={Styles.searchInput} ></input>
-      <button className={Styles.searchButton}>Search</button>
+        <input
+          type="text"
+          placeholder="Search by name..."
+          className={Styles.searchInput}
+        ></input>
+        <button className={Styles.searchButton}>Search</button>
       </div>
+      <button className={Styles.showAllButton}>Show All</button>
     </div>
   );
 };
