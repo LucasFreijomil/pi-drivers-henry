a09df5447818d38a9437593d161a1f10a40f771b
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-10-31 15:01:24 -0300

    models-created

--- server/src/models/Driver.js
+++ server/src/models/Driver.js
@@ -1,12 +1,41 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('Driver', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "Driver",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      dob: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
     },
-  });
+    { timestamps: false }
+  );
 };
