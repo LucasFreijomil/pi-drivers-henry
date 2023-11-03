c23f5ac1908960fdafde739345e0969c418794ab
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-11-03 18:32:38 -0300

    GET /drivers/:idDriver route and controller

---
+++ server/src/controllers/getDriverDetail.js
@@ -0,0 +1,25 @@
+const axios = require("axios");
+const { Driver } = require("../models/Driver");
+
+const URL = "http://localhost:5000/drivers";
+
+const getDriverDetail = async (req, res) => {
+  const { idDriver } = req.params;
+
+  try {
+    const dbDriver = await Driver.findByPk(idDriver);
+
+    if (dbDriver) {
+      return res.status(200).json(dbDriver);
+    } else {
+      const apiResponse = await axios.get(`${URL}/${idDriver}`);
+      const apiDriver = apiResponse.data;
+
+      return res.status(200).json(apiDriver);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+module.exports = getDriverDetail;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
-const getAllDrivers = require ("../controllers/getAllDrivers");
+const getAllDrivers = require("../controllers/getAllDrivers");
+const getDriverDetail = require("../controllers/getDriverDetail");
 const router = Router();
 
 router.get("/drivers", getAllDrivers);
+router.get("/drivers/:idDriver", getDriverDetail);
 
 module.exports = router;
