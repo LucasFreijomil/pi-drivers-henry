89d135f22415245bee9b02842f304bce5e5905e6
Lucas Freijomil <Lucas@MacBook-Pro-de-admin.local>
2023-11-08 09:43:53 -0300

    some changes

--- server/src/controllers/getDriverDetail.js
+++ server/src/controllers/getDriverDetail.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const { Driver } = require("../db");
+const { Driver, Team } = require("../db");
 
 const URL = "http://localhost:5000/drivers";
 
@@ -7,22 +7,44 @@ const getDriverDetail = async (req, res) => {
   const { idDriver } = req.params;
 
   try {
-    const dbDriver = await Driver.findByPk(idDriver);
+    const dbResponse = await Driver.findByPk(idDriver, {
+      include: { model: Team, attributes: ["name"], through: { attributes: [] } }
+    });
 
-    if (dbDriver) {
+    if (dbResponse) {
+      const teamsArray = dbResponse.Teams.map((team) => {
+        return { name: team.name };
+      });
+
+      const dbDriver = {
+        id: dbResponse.id,
+        name: {
+          forename: dbResponse.forename,
+          surname: dbResponse.surname
+        },
+        image: {
+          url: dbResponse.image
+        },
+        dob: dbResponse.dob,
+        nationality: dbResponse.nationality,
+        teams: teamsArray.map((team) => team.name).join(", "),
+        description: dbResponse.description
+      };
+
       return res.status(200).json(dbDriver);
     } else {
       const apiResponse = await axios.get(`${URL}/${idDriver}`);
       const apiDriver = apiResponse.data;
 
       return res.status(200).json(apiDriver);
-    } 
+    }
   } catch (error) {
-    if (error.response.status === 404)
+    if (error.response && error.response.status === 404)
       return res.status(404).json({ error: "Driver not found" });
 
     return res.status(500).send(error.message);
   }
 };
+
 
 module.exports = getDriverDetail;
